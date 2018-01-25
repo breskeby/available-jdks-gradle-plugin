45eee4d6fd0949cf4d62624c3cda56b7f221c709
Rene Groeschke <rene@gradle.com>
2018-01-26 00:04:06 +0100

    dont set parent id explicitly when using subprojects in tc dsl

--- .teamcity/JdkGradlePlugins/projects/BranchProject.kt
+++ .teamcity/JdkGradlePlugins/projects/BranchProject.kt
@@ -4,23 +4,17 @@ import JdkGradlePlugins.buildTypes.JdkGradlePlugins_Branch
 import jetbrains.buildServer.configs.kotlin.v2017_2.Project
 
 
-class BranchProject(branch:String, uuid:String) : Project({
+class BranchProject(branch: String, uuid: String) : Project({
     this.uuid = uuid
     id = "JdkGradlePlugins_$branch"
     parentId = "JdkGradlePlugins"
     name = branch
 
     subProjects(Project({
-        this.uuid = "${uuid}-stages"
-        id = "JdkGradlePlugins_${branch}_Stages"
-        parentId = "${BranchProject::id}"
-        name = "Stages"
-
-        buildType(JdkGradlePlugins_Branch(uuid = uuid + "_$branch", branch = branch))
-
-    }
-
-    ))
-
-
+            this.uuid = "${uuid}-stages"
+            id = "JdkGradlePlugins_${branch}_Stages"
+            name = "Stages"
+            buildType(JdkGradlePlugins_Branch(uuid = uuid + "_$branch", branch = branch))
+        })
+    )
 })
