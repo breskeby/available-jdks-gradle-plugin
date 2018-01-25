94e7a2b8228c4a6ef5d649c6fb1a8b5402b684fd
Rene Groeschke <rene@gradle.com>
2018-01-26 00:10:55 +0100

    fix stage project dsl error

--- .teamcity/JdkGradlePlugins_Stages/Project.kt
+++ .teamcity/JdkGradlePlugins_Stages/Project.kt
@@ -1,12 +1,11 @@
 package JdkGradlePlugins_Stages
 
-import JdkGradlePlugins_Stages.buildTypes.*
-import jetbrains.buildServer.configs.kotlin.v2017_2.*
+import JdkGradlePlugins_Stages.buildTypes.JdkGradlePlugins_Stages_Tested
 import jetbrains.buildServer.configs.kotlin.v2017_2.Project
 
 object Project : Project({
     uuid = "c4e4e922-aa09-49f7-9cd5-024e27b63bbc"
-    id = "JdkGradlePlugins_Stages"
+    id = "JdkGradlePlugins_Stages2"
     parentId = "JdkGradlePlugins"
     name = "Stages"
 
