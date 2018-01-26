d966ca74e7528a09f823ad61aa45fdc64a34ab32
Rene Groeschke <rene@gradle.com>
2018-01-26 13:45:32 +0100

    fix vcs root settings

--- .teamcity/JdkGradlePlugins/Project.kt
+++ .teamcity/JdkGradlePlugins/Project.kt
@@ -4,6 +4,7 @@ import JdkGradlePlugins.buildTypes.JdkGradlePlugins_FuncTest
 import JdkGradlePlugins.buildTypes.JdkGradlePlugins_UnitTest
 import JdkGradlePlugins.projects.BranchProject
 import _Root.vcsRoots.JdkGradlePluginsVcsRootMaster
+import _Root.vcsRoots.JdkGradlePluginsVcsRootRelease
 import jetbrains.buildServer.configs.kotlin.v2017_2.Project
 
 object Project : Project({
@@ -17,7 +18,7 @@ object Project : Project({
 
     subProjects(
             BranchProject("master", JdkGradlePluginsVcsRootMaster, "165EA6F9-C2BD-436E-B1E3-FBE59C3A308C"),
-            BranchProject("release", "C8937837-F200-4C21-8AD5-7C5484BC93AB")
+            BranchProject("release",JdkGradlePluginsVcsRootRelease, "C8937837-F200-4C21-8AD5-7C5484BC93AB")
     )
 
 })
